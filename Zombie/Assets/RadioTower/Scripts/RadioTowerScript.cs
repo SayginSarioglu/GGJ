9cb08197f1cfd15d2dee574f7fa1321e43b00366
ebubekir <ebubekir.karabuga@pixofun.com>
2018-01-27 01:36:13 +0300

    tower and etan

---
+++ Zombie/Assets/RadioTower/Scripts/RadioTowerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioTowerScript : MonoBehaviour {
+    float blinkSpeed = 1.0f;
+    float secondsPassed = 0f;
+    bool positive = false;
+
+    public Light towerLight;
+	void Update ()
+    {
+        if (positive)
+            secondsPassed += Time.deltaTime;
+        else
+            secondsPassed -= Time.deltaTime;
+
+        if ( secondsPassed > 2f)
+        {
+            secondsPassed = 2f;
+            positive = !positive;
+        }
+        if ( secondsPassed < -0.5f)
+        {
+            secondsPassed = -0.5f;
+            positive = !positive;
+        }
+        
+
+        towerLight.intensity = secondsPassed;
+	}
+}
