639ec3740ce9b769524fbc66d51df26fb5a3e631
emrepixofun <emre.akin@pixofun.com>
2018-01-27 16:04:46 +0300

    re

--- Zombieee/Assets/SamuzaiController.cs
+++ Zombieee/Assets/SamuzaiController.cs
@@ -2,22 +2,49 @@
 
 public class SamuzaiController : MonoBehaviour {
 
-	void Update ()
+    public Animator animator;
+
+    int prevType = 0;
+    int type = 0;
+
+    void Update ()
     {
         float x = 0;
         float z = 0;
 
         if (Input.GetKey("left"))
             x = -5;
-        if (Input.GetKey("right"))
+        else if (Input.GetKey("right"))
             x = 5;
 
         if (Input.GetKey("down"))
+        {
             z = -Time.deltaTime * 10;
-        if (Input.GetKey("up"))
+            animator.SetBool("walk", true);
+            type = 1;
+        }
+        else if (Input.GetKey("up"))
+        {
             z = Time.deltaTime * 10;
+            animator.SetBool("walk", true);
+            type = 1;
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+            type = 0;
+        }
 
-        if (Input.GetKeyDown("space")) ;
+        if (Input.GetKey("space"))
+        {
+            animator.SetBool("attack", true);
+            type = 2;
+        }
+        else
+            animator.SetBool("attack", false);
+
+        if(prevType != type)
+            prevType = type;
 
         transform.Rotate(new Vector3(0, x, 0));
         transform.Translate(new Vector3(0, 0, z));
