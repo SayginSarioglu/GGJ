f9299f4254ebd01ff2282715e73b3d0b4f6e9933
SayginSarioglu <e181952@metu.edu.tr>
2018-01-27 22:17:40 +0300

    bismillahirrahmanirrahim

---
+++ Zombieee/Assets/VolumetricLines/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+
+    public GameObject samurai;
+	// Use this for initialization
+	void Start () {
+        samurai = GameObject.Find("samuzai");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * 30f;
+
+        if (Vector3.Distance(transform.position, samurai.transform.position) < 2)
+        {
+            samurai.transform.position = new Vector3(Random.Range(-10, 10), samurai.transform.position.y, Random.Range(-10, 10));
+            Utility.guiManager.ethanScoreText.text = "Ethan: " + ++Utility.guiManager.ethanScore;
+            Destroy(gameObject);
+        }
+    }
+}
